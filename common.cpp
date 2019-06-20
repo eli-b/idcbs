0ade665173d1d03be8996d44c4230e8bb50b49c2
Eli Boyarski <eli@boyar.ski>
2019-06-20 17:16:08 +0300

    Constraint and conflict <<-operators assume grid is two-dimensional and a hard-coded number of rows for greater readability

--- common.cpp
+++ common.cpp
@@ -1,10 +1,15 @@
 #include "common.h"
 
+constexpr int GRID_ROWS = 8;
+
 std::ostream& operator<<(std::ostream& os, const Constraint& constraint)
 {
-	os << "<" << get<0>(constraint) << ", " << get<1>(constraint)
-		<< ", " << get<2>(constraint);
-	if (get<3>(constraint))
+    auto [loc1, loc2, timestep, positive_constraint] = constraint;
+    if (loc2 == -1)
+        os << "<(" << loc1 / GRID_ROWS << "," << loc1 % GRID_ROWS << "), " << timestep;
+    else
+        os << "<(" << loc1 / GRID_ROWS << "," << loc1 % GRID_ROWS << "), (" << loc2 / GRID_ROWS << "," << loc2 % GRID_ROWS << "), " << timestep;
+	if (positive_constraint)
 		os << ", positive>";
 	else
 		os << ", negative>";
@@ -13,7 +18,11 @@ std::ostream& operator<<(std::ostream& os, const Constraint& constraint)
 
 std::ostream& operator<<(std::ostream& os, const Conflict& conflict)
 {
-	os << "<" << get<0>(conflict) << ", " << get<1>(conflict) << ","
-		<< get<2>(conflict) << ", " << get<3>(conflict) << ", " << get<4>(conflict) << ">";
+    auto [agent1, agent2, loc1, loc2, timestep] = conflict;
+    if (loc2 == -1)
+        os << "<" << agent1 << ", " << agent2 << ", (" << loc1 / GRID_ROWS << "," << loc1 % GRID_ROWS << "), " << timestep << ">";
+    else
+        os << "<" << agent1 << ", " << agent2 << ", (" << loc1 / GRID_ROWS << "," << loc1 % GRID_ROWS << "), ("
+                                                       << loc2 / GRID_ROWS << "," << loc2 % GRID_ROWS << "), " << timestep << ">";
 	return os;
 }
